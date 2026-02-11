c04169665b79fbb8b459a8b38dc162430b426f6e
Cogander <ivanderchristy13@gmail.com>
2026-02-11 21:37:38 +0700

    Create authMiddleware.js

---
+++ config/models/controllers/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+exports.protect = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "Tidak ada token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token.split(" ")[1], process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "Token tidak valid" });
+  }
+};
+
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Akses ditolak" });
+    }
+    next();
+  };
+};
